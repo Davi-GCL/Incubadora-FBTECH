3ee5791709991a27e803f0649c7973adb04123f9
Davi-GCL <1davi.gabriel1@gmail.com>
2023-01-11 21:39:06 -0300

    fix: reenvio de formulario

--- cadastro.php
+++ cadastro.php
@@ -12,17 +12,32 @@ if(isset($_POST['nome']) || isset($_POST['email']) || isset($_POST['senha'])){
     // Encriptar a senha antes de armazenar no banco de dados
     // $senha = password_hash($senha, PASSWORD_DEFAULT);
     
-    // Inserir dados na tabela
-    $sql = "INSERT INTO usuarios (nome, email, senha)
-    VALUES ('$nome', '$email', '$senha')";
-    
-    if ($mysqli->query($sql) === TRUE) {
-        if(strlen($_POST['nome']) && strlen($_POST['senha']) != 0){
-            echo "<script> alert('Usuário registrado com sucesso'); </script>";
+//Algoritmo para evitar que aconteca o reenvio de formulario
+
+    //Codigo SQL
+    $sql_code = "SELECT * FROM usuarios WHERE nome = '$nome' AND email = '$email'";
+    //Consulta no banco de dados
+    $sql_query = $mysqli->query($sql_code) or die("falha na execucao do codigo SQL: ".$mysqli->error);
+
+    $quantidade = $sql_query->num_rows;
+
+    if($quantidade == 0){
+
+        // Insere os dados na tabela
+        $sql = "INSERT INTO usuarios (nome, email, senha)
+        VALUES ('$nome', '$email', '$senha')";
+        
+        if ($mysqli->query($sql) === TRUE) {
+            if(strlen($_POST['nome']) && strlen($_POST['senha']) != 0){
+                echo "<script> alert('Usuário registrado com sucesso'); </script>";
+            }
+        } else {
+            echo "Erro: " . $sql . "<br>" . $mysqli->error;
         }
-    } else {
-        echo "Erro: " . $sql . "<br>" . $mysqli->error;
-    }    
+        
+    }else {
+        echo "Este usuario já foi cadastrado!";
+    }
 
 }
 
@@ -79,6 +94,8 @@ $mysqli->close();
                     <input placeholder="Confirme a senha" class="login-form" id="conf-senha" name="conf-senha" type="password" required>
                     <span class="show-btn" onclick="mostrar_senha(this, '#conf-senha')"></span>
                 </div>
+                
+                <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
 
                 <div class="login-area-button">
                     <a href="login.php">Ja Tenho Cadastro</a>
